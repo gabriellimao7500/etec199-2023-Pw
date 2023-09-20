ced2ca539533465c9a1e360a1708be803b6f39ac
gabriellimao7500 <gabriellimao7500@gmail.com>
2023-09-20 11:19:25 -0300

    update turn theme

--- Geek/JavaScript/hamburger.js
+++ Geek/JavaScript/hamburger.js
@@ -1,34 +1,5 @@
 //auteração de tema
-const background = document.querySelector(".background") 
-const sun = document.querySelector(".sun-none")
-const city = document.querySelector(".city-none")
-const button_theme = document.querySelector(".theme-alternateS")
 
-var theme = "white"
-
-function turn_theme(){
-    if (theme == "white"){
-        theme = "dark"
-        background.classList.toggle("darkmode")
-        sun.classList.toggle("mon")
-        city.classList.toggle("city-on")
-        button_theme.classList.toggle("theme-alternateM")
-        setTimeout(function(){
-            sun.classList.toggle("mon")
-            city.classList.toggle("city-on")
-        },4000)
-    }else{
-        theme = "white"
-        background.classList.toggle("darkmode")
-        sun.classList.toggle("sun")
-        city.classList.toggle("city-on")
-        button_theme.classList.toggle("theme-alternateM")    
-        setTimeout(function(){
-            sun.classList.toggle("sun")
-            city.classList.toggle("city-on")
-        },4000)
-    }
-}
 
 //menu hamburguer
 const hamburger = document.querySelector(".hamburguer");
